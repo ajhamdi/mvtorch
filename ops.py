387b30c041225bbf465a184a30a64342ec65dbb6
Faisal Alzahrani <fkhalhassani@gmail.com>
2022-08-17 16:26:25 +0300

    Added multi-view aggregators and their util and ops functions

--- ops.py
+++ ops.py
@@ -4,6 +4,13 @@ from pytorch3d.renderer import look_at_view_transform
 
 import sys
 
+from einops import rearrange
+
+def mvctosvc(x): 
+    return rearrange(x, 'b m c h w -> (b m) c h w ')
+def svctomvc(x,nb_views=1): 
+    return rearrange(x, '(b m) c h w -> b m c h w',m=nb_views)
+
 def check_and_correct_rotation_matrix(R, T, nb_trials, azim, elev, dist):
     exhastion = 0
     while not check_valid_rotation_matrix(R):
@@ -13,3 +20,45 @@ def check_and_correct_rotation_matrix(R, T, nb_trials, azim, elev, dist):
             if not check_valid_rotation_matrix(R) and exhastion > nb_trials:
                 sys.exit("Remedy did not work")
     return R , T
+
+def batched_index_select_(x, idx):
+    """
+    This can be used for neighbors features fetching
+    Given a pointcloud x, return its k neighbors features indicated by a tensor idx.
+    :param x: torch.Size([batch_size, num_dims, num_vertices, 1])
+    :param index: torch.Size([batch_size, num_vertices, k])
+    :return: torch.Size([batch_size, num_dims, num_vertices, k])
+    """
+
+    batch_size, num_dims, num_vertices = x.shape[:3]
+    _, all_combo, k = idx.shape
+    idx_base = torch.arange(
+        0, batch_size, device=idx.device).view(-1, 1, 1) * num_vertices
+    idx = idx + idx_base
+    idx = idx.view(-1)
+
+    x = x.transpose(2, 1).contiguous()
+    feature = x.view(batch_size * num_vertices, -1)[idx, :]
+    feature = feature.view(batch_size, all_combo, k,
+                           num_dims).permute(0, 3, 1, 2)
+    return feature
+
+
+def batched_index_select_parts(x, idx):
+    """
+    This can be used for neighbors features fetching
+    Given a pointcloud x, return its k neighbors features indicated by a tensor idx.
+    :param x: torch.Size([batch_size, num_vertices , 1])
+    :param index: torch.Size([batch_size, num_views, points_per_pixel,H,W])
+    :return: torch.Size([batch_size, _vertices, k])
+    """
+
+    batch_size, num_view, num_nbrs, H, W = idx.shape[:5]
+    _, num_dims, num_vertices = x.shape
+
+    idx = rearrange(idx, 'b m p h w -> b (m h w) p')
+    x = x[..., None]
+    feature = batched_index_select_(x, idx)
+    feature = rearrange(feature, 'b d (m h w) p -> b m d p h w',
+                        m=num_view, h=H, w=W, d=num_dims)
+    return feature
--- util.py
+++ util.py
@@ -185,3 +185,71 @@ def torch_direction_vector(azim, elev, from_degrees=True):
     dir_vector[:, 1] = torch.sin(elev)
     dir_vector[:, 2] = torch.cos(azim) * torch.cos(elev)
     return dir_vector
+
+def class_freq_to_weight(class_freqs, alpha=1.0):
+    """
+    a function to convert  a dictionary of labels frequency  to dictionary of loss weights per class label that are averaged to 1. This is helpful in designing a weighted loss 
+    """
+    total = 0
+    result_weights = {}
+    cls_nbrs = len(class_freqs)
+    for k, v in class_freqs.items():
+        total += v
+    avg = total/float(cls_nbrs)
+    for k, v in class_freqs.items():
+        result_weights[k] = alpha * avg/v + (1-alpha)
+    return result_weights
+
+def labels2freq(label_map):
+    """
+    a torch util funtion to return dict with frquencies of labels in the given examples in the case of dense labels.
+    """
+    lbl, inv_map, freq = torch.unique(
+        label_map, return_counts=True, sorted=True, return_inverse=True)
+
+    return {k: v for k, v in zip(lbl.detach().cpu().numpy().tolist(), freq.detach().cpu().numpy().tolist())}, inv_map
+
+# from the nerf paper 
+def positional_encoding(
+    tensor, num_encoding_functions=0, include_input=True, log_sampling=True
+) -> torch.Tensor:
+    r"""Apply positional encoding to the input.
+    Args:
+        tensor (torch.Tensor): Input tensor to be positionally encoded.
+        encoding_size (optional, int): Number of encoding functions used to compute
+            a positional encoding (default: 6).
+        include_input (optional, bool): Whether or not to include the input in the
+            positional encoding (default: True).
+    Returns:
+    (torch.Tensor): Positional encoding of the input tensor.
+    """
+    # TESTED
+    # Trivially, the input tensor is added to the positional encoding.
+    encoding = [tensor] if include_input else []
+    frequency_bands = None
+    if log_sampling:
+        frequency_bands = 2.0 ** torch.linspace(
+            0.0,
+            num_encoding_functions - 1,
+            num_encoding_functions,
+            dtype=tensor.dtype,
+            device=tensor.device,
+        )
+    else:
+        frequency_bands = torch.linspace(
+            2.0 ** 0.0,
+            2.0 ** (num_encoding_functions - 1),
+            num_encoding_functions,
+            dtype=tensor.dtype,
+            device=tensor.device,
+        )
+
+    for freq in frequency_bands:
+        for func in [torch.sin, torch.cos]:
+            encoding.append(func(tensor * freq))
+
+    # Special case, for no positional encoding
+    if len(encoding) == 1:
+        return encoding[0]
+    else:
+        return torch.cat(encoding, dim=-1)
