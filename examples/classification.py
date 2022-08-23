0343ab4dd957677fb7dad24fc8050c18e8fc275e
Faisal Alzahrani <fkhalhassani@gmail.com>
2022-08-23 14:50:31 +0300

    Moved examples to separate folder

--- examples/classification.py
+++ examples/classification.py
@@ -1,3 +1,9 @@
+# to import files from parent dir
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import torch
 from torch import nn
 from mvtorch.datasets import ScanObjectNN, collate_fn
