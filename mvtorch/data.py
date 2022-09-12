ef11c6781900c8aba5db3edb2f2974559eac2fe1
Faisal Alzahrani <fkhalhassani@gmail.com>
2022-09-12 22:27:35 +0300

    Added new ShapeNetPart dataset class

--- mvtorch/data.py
+++ mvtorch/data.py
@@ -901,3 +901,74 @@ class CustomDataLoader(torch.utils.data.DataLoader):
     def __init__(self, *args, **kwargs) -> None:
         kwargs['collate_fn'] = collate_fn
         super().__init__(*args, **kwargs)
+
+def load_data_partseg(root_dir, partition):
+    all_data = []
+    all_label = []
+    all_seg = []
+    if partition == 'trainval':
+        file = glob.glob(os.path.join(root_dir, '*train*.h5')) \
+               + glob.glob(os.path.join(root_dir, '*val*.h5'))
+    else:
+        file = glob.glob(os.path.join(root_dir, '*%s*.h5'%partition))
+    for h5_name in file:
+        f = h5py.File(h5_name, 'r+')
+        data = f['data'][:].astype('float32')
+        label = f['label'][:].astype('int64')
+        seg = f['pid'][:].astype('int64')
+        f.close()
+        all_data.append(data)
+        all_label.append(label)
+        all_seg.append(seg)
+    all_data = np.concatenate(all_data, axis=0)
+    all_label = np.concatenate(all_label, axis=0)
+    all_seg = np.concatenate(all_seg, axis=0)
+    return all_data, all_label, all_seg
+
+
+def translate_pointcloud(pointcloud):
+    xyz1 = np.random.uniform(low=2./3., high=3./2., size=[3])
+    xyz2 = np.random.uniform(low=-0.2, high=0.2, size=[3])
+       
+    translated_pointcloud = np.add(np.multiply(pointcloud, xyz1), xyz2).astype('float32')
+    return translated_pointcloud
+
+class ShapeNetPart(Dataset):
+    def __init__(self, root_dir, num_points=2500, partition='train', class_choice=None):
+        self.data, self.label, self.seg = load_data_partseg(root_dir, partition)
+        self.cat2id = {'airplane': 0, 'bag': 1, 'cap': 2, 'car': 3, 'chair': 4, 
+                       'earphone': 5, 'guitar': 6, 'knife': 7, 'lamp': 8, 'laptop': 9, 
+                       'motor': 10, 'mug': 11, 'pistol': 12, 'rocket': 13, 'skateboard': 14, 'table': 15}
+        self.seg_num = [4, 2, 2, 4, 4, 3, 3, 2, 4, 2, 6, 2, 3, 3, 3, 3]
+        self.index_start = [0, 4, 6, 8, 12, 16, 19, 22, 24, 28, 30, 36, 38, 41, 44, 47]
+        self.root_dir = root_dir
+        self.num_points = num_points
+        self.partition = partition        
+        self.class_choice = class_choice
+
+        if self.class_choice != None:
+            id_choice = self.cat2id[self.class_choice]
+            indices = (self.label == id_choice).squeeze()
+            self.data = self.data[indices]
+            self.label = self.label[indices]
+            self.seg = self.seg[indices]
+            self.seg_num_all = self.seg_num[id_choice]
+            self.seg_start_index = self.index_start[id_choice]
+        else:
+            self.seg_num_all = 50
+            self.seg_start_index = 0
+
+    def __getitem__(self, item):
+        pointcloud = self.data[item][:self.num_points]
+        label = self.label[item]
+        seg = self.seg[item][:self.num_points]
+        if self.partition == 'trainval':
+            pointcloud = translate_pointcloud(pointcloud)
+            indices = list(range(pointcloud.shape[0]))
+            np.random.shuffle(indices)
+            pointcloud = pointcloud[indices]
+            seg = seg[indices]
+        return pointcloud, label, seg, self.index_start[label], self.seg_num[label]
+
+    def __len__(self):
+        return self.data.shape[0]
