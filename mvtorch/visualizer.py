7a834ff524897bdb48c4c3ac72ff909c48edf129
Faisal Alzahrani <fkhalhassani@gmail.com>
2022-11-01 00:40:25 +0300

    Updated visualizer to support mesh visualization

--- mvtorch/visualizer.py
+++ mvtorch/visualizer.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 from mvtorch.mvrenderer import MVRenderer
 
-class PointCloudVisualizer():
-    
-    def __init__(self, nb_views, image_size=224, object_color=(1, 1, 1), background_color=(0, 0, 0), points_radius=0.006, points_per_pixel=1):
+class Visualizer():
+    def __init__(self, nb_views, pc_rendering, image_size=224, object_color=(1, 1, 1), background_color=(0, 0, 0), points_radius=0.006, points_per_pixel=1):
         self.nb_views = nb_views
+        self.pc_rendering = pc_rendering
         self.image_size = image_size
         self.object_color = object_color
         self.background_color = background_color
@@ -14,20 +14,16 @@ class PointCloudVisualizer():
         self.renderer = MVRenderer(
             nb_views=self.nb_views,
             image_size=self.image_size,
-            pc_rendering=True,
+            pc_rendering=self.pc_rendering,
             object_color=self.object_color,
             background_color=self.background_color,
             points_per_pixel=self.points_per_pixel,
             return_mapping=False
         )
 
-    def visualize_inline(self, points, dist, elev, azim):
-        
-        rendered_images, _ = self.renderer(None, points, azim=azim, elev=elev, dist=dist, color=self.object_color)
-
-        batch_size = rendered_images.shape[0]
-        nb_views = rendered_images.shape[1]
-        
+    def visualize_inline(self, meshes, points, dist, elev, azim):
+        rendered_images, _ = self.renderer(meshes, points, azim=azim, elev=elev, dist=dist, color=self.object_color)
+        batch_size, nb_views = rendered_images.shape[0], rendered_images.shape[1]
         rendered_images = rendered_images.cpu()
 
         fig, axs = plt.subplots(nrows=batch_size, ncols=nb_views, squeeze=False, figsize=(4*nb_views, 4*batch_size))
